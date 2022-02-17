30d2bfccdc9cfcb72153d84349e54695c1b0625a
ahmedadel56 <ahmdadl563@gmail.com>
2022-02-17 16:35:00 +0200

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :update_allowed_parameters, if: :devise_controller?
-  
-    protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:email, :password, :current_password)
-      end
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :password, :current_password)
     end
   end
+end
